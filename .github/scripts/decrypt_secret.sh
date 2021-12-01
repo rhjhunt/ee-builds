6a79935c7b06dcc26c63ed11a475ba101915e084
Jacob Hunt <384643+rhjhunt@users.noreply.github.com>
2021-12-01 12:52:09 -0700

    added satellite build

--- .github/scripts/decrypt_secret.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# --batch to prevent interactive command
-# --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$PASSPHRASE" \
---output $EE_FOLDER_NAME/ansible.cfg ansible.cfg.gpg
